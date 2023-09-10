61ce32deab20dbac0f2a10df116da6b0072bab0a
SubakshaAnand <subakshaanand7@gmail.com>
2023-09-10 16:15:08 +0530

    login page

--- dev-reactjs/src/App.jsx
+++ dev-reactjs/src/App.jsx
@@ -4,22 +4,24 @@ import './App.css';
 //importing pages from difffe
 import Dashboard from './pages/dashboard/dashboard';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
+import LoginPage from './pages/login-page/login';
 
 function App() {
   return (
     <>
-  <Router>
-    <div>
-      <ul>
-        <li>
-          <Link to='/'>Dashboard</Link>
-        </li>
-      </ul>
-    </div>
-    <Routes>
-<Route exact path='/' element={< Dashboard/>}></Route>
-</Routes>
-  </Router>
+      <Router>
+        {/* <div>
+          <ul>
+            <li>
+              <Link to='/'>Dashboard</Link>
+            </li>
+          </ul>
+        </div> */}
+        <Routes>
+          <Route exact path='/' element={< LoginPage />}></Route>
+          <Route exact path='/dashboard' element={<Dashboard />}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
---
+++ dev-reactjs/src/pages/login-page/login.jsx
@@ -0,0 +1,22 @@
+import { Card, CardHeader,
+    CardBody,
+    CardFooter,
+    Typography,
+    Input,
+    Checkbox,
+    Button } from "@material-tailwind/react";
+import React from "react";
+
+
+export default function LoginPage(){
+
+return (
+
+<>
+<Card className="mt-6 w-96">
+<div>Tamil</div>
+</Card>
+</>
+
+);
+};
